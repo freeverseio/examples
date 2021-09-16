6e640023b82258446903f0663a24acc2dcc0a13c
Toni Mateos <tmateos@freeverse.io>
2021-09-16 14:22:27 +0200

    improving example

--- nodejs/identity_decrypt.js
+++ nodejs/identity_decrypt.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-/* eslint-disable camelcase */
 // MIT License
 
 // Copyright (c) 2021 freeverse.io
@@ -23,23 +22,24 @@
 // SOFTWARE.
 
 const identity = require('freeverse-crypto-js');
-const assert = require('assert');
+const argv = require('minimist')(process.argv.slice(2), { string: ['encrypted', 'password'] });
 
-// get and check arguments
-const args = process.argv.slice(2);
-assert(args.length > 1, 'You must provide an encrypted ID and password as separate arguments.');
-
-// PASTE REQUIRED VARIABLES HERE
-const encrypted = args[0];
-const password = args[1];
-
+const { password, encrypted } = argv;
+if (!password || !encrypted) {
+  console.log(`
+    ---------------
+    Usage Example: 
+    node identity_decrypt.js --password 'P@ssw0rd' --encrypted 'c33dcc598252fbbb4a94ff2d0f70dbe7d77360d8ca4a036ad1dd80bc4c7bb0b818517bdd5fdd0cf0562080e33559bfab637d3ed3ccd6ddfdbd58b8d8874047bf'
+    ---------------
+    `);
+} else {
 // this is the user's private key
-const pvk = identity.decryptIdentity(encrypted, password);
+  const pvk = identity.decryptIdentity(encrypted, password);
 
-// this is the freeverseID (public address) of the user
-const freeverseID = identity.freeverseIdFromPrivateKey(pvk);
+  // this is the freeverseID (public address) of the user
+  const freeverseID = identity.freeverseIdFromPrivateKey(pvk);
 
-const output = `
+  console.log(`
 ---------------
 Original encrypted ID:
 ${encrypted}
@@ -49,6 +49,5 @@ Private Key:
 ${pvk}
 FreeverseID (public address):
 ${freeverseID}
----------------`;
-
-console.log(output);
+---------------`);
+}
