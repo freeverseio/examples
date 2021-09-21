d7a6b995744c92861276e08fa0aa96c803cda85e
Alun Evans <alun@alunthomasevans.co.uk>
2021-09-21 17:38:40 +0200

    All examples working, readme updated

--- nodejs/asset_create.js
+++ nodejs/asset_create.js
@@ -24,64 +24,88 @@
 
 const identity = require('freeverse-crypto-js');
 const signer = require('freeverse-apisigner-js');
-const assert = require('assert');
+const argv = require('minimist')(process.argv.slice(2), { string: ['pvk', 'owner', 'uni', 'nonce'] });
 
-// PASTE REQUIRED VARIABLES FOR MUTATION HERE
-const universe_owner_pvk = ''; // universe account owner private key
-const new_asset_owner_id = ''; // FreeverseID of user receiving asset
-const owner_nonce_value = 0; // see https://docs.livingassets.io/quickstart/developer_quickstart_2/#example-querying-a-user-nonce-value
-const universe_id = 0; // your universe id (provided by us)
-const new_asset_props = { // properties for asset following standard https://docs.livingassets.io/api/props_standard/
-  name: 'Supercool Dragon',
-  description: 'Legendary creature that loves fire.',
-  image: 'ipfs://QmPAg1mjxcEQPPtqsLoEcauVedaeMH81WXDPvPx3VC5zUz',
-  attributes: [
-    {
-      trait_type: 'Rarity',
-      value: 'Scarce',
-    },
-    {
-      trait_type: 'Level',
-      value: 5,
-    },
-    {
-      trait_type: 'Weight',
-      value: 123.5,
-    },
-  ],
-};
-const new_asset_metadata = { // any metadata you want
-  private_data: 'that only I will see',
-};
+const {
+  pvk, owner, uni, nonce,
+} = argv;
 
-assert(universe_owner_pvk !== '', 'Please enter a private key to run this example!');
-assert(new_asset_owner_id !== '', 'Please enter an public address to run this example!');
+if (!pvk || !owner || !uni || !nonce) {
+  console.log(`
+    ---------------
+    Usage Example: 
+    node asset_create.js --pvk '0xd2827f4c3778758eb51719a698464aaffd10a5c7cf816c1de83c5e446bfc8e8d' --owner '0x983c1A8FCf74CAF648fD51fd7A6d322a502ae789' --uni '0' --nonce '0'
+    ---------------
+    `);
+} else {
+  const universe_owner_pvk = pvk; // private key of universe owner
+  const new_asset_owner_id = owner; // new owner of created asset
+  const owner_nonce_value = nonce; // see https://docs.livingassets.io/quickstart/developer_quickstart_2/#example-querying-a-user-nonce-value
+  const universe_id = uni; // your universe id (provided by us)
+  const new_asset_props = { // properties for asset following standard https://docs.livingassets.io/api/props_standard/
+    name: 'Supercool Dragon',
+    description: 'Legendary creature that loves fire.',
+    image: 'ipfs://QmPAg1mjxcEQPPtqsLoEcauVedaeMH81WXDPvPx3VC5zUz',
+    attributes: [
+      {
+        trait_type: 'Rarity',
+        value: 'Scarce',
+      },
+      {
+        trait_type: 'Level',
+        value: 5,
+      },
+      {
+        trait_type: 'Weight',
+        value: 123.5,
+      },
+    ],
+  };
+  const new_asset_metadata = { // any metadata you want
+    private_data: 'that only I will see',
+  };
 
-// create web3 account from your private key
-// (other forms of creating web3 account could be subsituted)
-const universe_owner_account = identity.createNewAccount(universe_owner_pvk);
+  // create web3 account from your private key
+  // (other forms of creating web3 account could be subsituted)
+  const universe_owner_account = identity.accountFromPrivateKey(universe_owner_pvk);
 
-// call function to create the operation and sign it with your account
-const result = signer.createAssetMutationInputs({
-  universeOwnerAccount: universe_owner_account,
-  newAssetOwnerId: new_asset_owner_id,
-  userNonce: owner_nonce_value,
-  universeIdx: universe_id,
-  propsJSON: new_asset_props,
-  metadataJSON: new_asset_metadata,
-});
+  // call function to create the operation and sign it with your account
+  const result = signer.createAssetMutationInputs({
+    universeOwnerAccount: universe_owner_account,
+    newAssetOwnerId: new_asset_owner_id,
+    userNonce: owner_nonce_value,
+    universeIdx: universe_id,
+    propsJSON: new_asset_props,
+    metadataJSON: new_asset_metadata,
+  });
 
-// inject results into final mutation to send to graphQL endpoint
-const assetMutation = `
-  mutation { 
-      execute(
-        input: { 
-          ops: ["${result.ops}"], 
-          signature: "${result.signature}",
-          universe: 0,
-        }){
-          results 
-        }}
-  `;
+  // inject results into final mutation to send to graphQL endpoint
+  const assetMutation = `
+mutation { 
+    execute(
+      input: { 
+        ops: ["${result.ops}"], 
+        signature: "${result.signature}",
+        universe: 0,
+      }
+    )
+  {
+    results 
+  }
+}
+    `;
 
-console.log(assetMutation);
+  console.log(`
+---------------
+Private key of universe owner:
+${universe_owner_pvk}
+Public address of new owner:
+${new_asset_owner_id}:
+Universe:
+${universe_id}
+User nonce value:
+${owner_nonce_value}
+GraphQL mutation:
+${assetMutation}
+  `);
+}
--- nodejs/asset_update.js
+++ nodejs/asset_update.js
@@ -24,64 +24,88 @@
 
 const identity = require('freeverse-crypto-js');
 const signer = require('freeverse-apisigner-js');
-const assert = require('assert');
+const argv = require('minimist')(process.argv.slice(2), { string: ['pvk', 'asset', 'uni', 'nonce'] });
 
-// PASTE REQUIRED VARIABLES FOR MUTATION HERE
-const universe_owner_pvk = ''; // universe account owner private key
-const asset_id = ''; // ID of asset to update
-const asset_nonce_value = 0; // see https://docs.livingassets.io/quickstart/developer_quickstart_2/#example-querying-a-user-nonce-value
-const universe_id = 0; // your universe id (provided by us)
-const updated_asset_props = { // properties for asset following standard https://docs.livingassets.io/api/props_standard/
-  name: 'Supercool Dragon',
-  description: 'Legendary creature that loves ice.',
-  image: 'ipfs://QmPAg1mjxcEQPPtqsLoEcauVedaeMH81WXDPvPx3VC5zUz',
-  attributes: [
-    {
-      trait_type: 'Rarity',
-      value: 'Common',
-    },
-    {
-      trait_type: 'Level',
-      value: 10,
-    },
-    {
-      trait_type: 'Weight',
-      value: 223.5,
-    },
-  ],
-};
-const updated_asset_metadata = { // any metadata you want
-  private_data: 'that has been updated',
-};
+const {
+  pvk, asset, uni, nonce,
+} = argv;
 
-assert(universe_owner_pvk !== '', 'Please enter a private key to run this example!');
-assert(asset_id !== '', 'Please enter an asset ID to run this example!');
+if (!pvk || !asset || !uni || !nonce) {
+  console.log(`
+    ---------------
+    Usage Example: 
+    node asset_update.js --pvk '0xd2827f4c3778758eb51719a698464aaffd10a5c7cf816c1de83c5e446bfc8e8d' --asset '1858476204501659870681251187806966130514471238263' --uni '0' --nonce '0'
+    ---------------
+    `);
+} else {
+  const universe_owner_pvk = pvk; // universe account owner private key
+  const asset_id = asset; // ID of asset to update
+  const asset_nonce_value = nonce; // see https://docs.livingassets.io/quickstart/developer_quickstart_2/#example-querying-a-user-nonce-value
+  const universe_id = uni; // your universe id (provided by us)
+  const updated_asset_props = { // properties for asset following standard https://docs.livingassets.io/api/props_standard/
+    name: 'Supercool Dragon',
+    description: 'Legendary creature that loves ice.',
+    image: 'ipfs://QmPAg1mjxcEQPPtqsLoEcauVedaeMH81WXDPvPx3VC5zUz',
+    attributes: [
+      {
+        trait_type: 'Rarity',
+        value: 'Common',
+      },
+      {
+        trait_type: 'Level',
+        value: 10,
+      },
+      {
+        trait_type: 'Weight',
+        value: 223.5,
+      },
+    ],
+  };
+  const updated_asset_metadata = { // any metadata you want
+    private_data: 'that has been updated',
+  };
 
-// create web3 account from your private key
-// (other forms of creating web3 account could be subsituted)
-const universe_owner_account = identity.createNewAccount(universe_owner_pvk);
+  // create web3 account from your private key
+  // (other forms of creating web3 account could be subsituted)
+  const universe_owner_account = identity.accountFromPrivateKey(universe_owner_pvk);
 
-// call function to create the operation and sign it with your account
-const result = signer.updateAssetMutationInputs({
-  universeOwnerAccount: universe_owner_account,
-  assetId: asset_id,
-  assetNonce: asset_nonce_value,
-  universeIdx: universe_id,
-  propsJSON: updated_asset_props,
-  metadataJSON: updated_asset_metadata,
-});
+  // call function to create the operation and sign it with your account
+  const result = signer.updateAssetMutationInputs({
+    universeOwnerAccount: universe_owner_account,
+    assetId: asset_id,
+    assetNonce: asset_nonce_value,
+    universeIdx: universe_id,
+    propsJSON: updated_asset_props,
+    metadataJSON: updated_asset_metadata,
+  });
 
-// inject results into final mutation to send to graphQL endpoint
-const assetMutation = `
+  // inject results into final mutation to send to graphQL endpoint
+  const assetMutation = `
   mutation { 
       execute(
         input: { 
           ops: ["${result.ops}"], 
           signature: "${result.signature}",
           universe: 0,
-        }){
-          results 
-        }}
+        }
+      )
+    {
+      results 
+    }
+  }
   `;
 
-console.log(assetMutation);
+  console.log(`
+---------------
+Private key of universe owner:
+${universe_owner_pvk}
+ID of asset to edit:
+${asset_id}:
+Universe:
+${universe_id}
+Asset nonce value:
+${asset_nonce_value}
+GraphQL mutation:
+${assetMutation}
+    `);
+}
