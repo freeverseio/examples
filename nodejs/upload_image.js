74c03d4f47f0b178f400e4d7ddb59876d96e44ac
Toni Mateos <tmateos@freeverse.io>
2021-09-27 14:00:07 +0200

    minor refactoring

--- nodejs/upload_image.js
+++ nodejs/upload_image.js
@@ -1,72 +1,81 @@
-const Web3 = require('web3');
+/* eslint-disable no-console */
+const identity = require('freeverse-crypto-js');
 const { createReadStream } = require('fs');
 const FormData = require('form-data');
 const fetch = require('cross-fetch');
 const Hash = require('ipfs-only-hash');
 const signer = require('freeverse-apisigner-js');
 
-const web3 = new Web3();
-const argv = require('minimist')(process.argv.slice(2), { string: ['pvk', 'path', 'uni', "api"] });
+const argv = require('minimist')(process.argv.slice(2), { string: ['pvk', 'path', 'universe', 'endpoint'] });
+
 const {
-    pvk, api, uni, path,
-  } = argv;
+  pvk, endpoint, universe, path,
+} = argv;
 
-  if (!pvk || !api || !uni || !path) {
-      throw new Error("Please check required parameters.")
+const checkArgs = () => {
+  const OK = pvk && universe && endpoint && path;
+  if (!OK) {
+    console.log(`
+    ---------------
+    Usage Example: 
+    node upload_image.js --pvk '0xd2827f4c3778758eb51719a698464aaffd10a5c7cf816c1de83c5e446bfc8e8d' --universe '0' --path 'imgs/image.png' --endpoint 'https://api.blackhole.gorengine.com'
+
+    params:
+    * pvk: the privateKey of the universe owner
+    * universe: the (uint) index of the universe
+    * endpoint: the API endpoint
+    * path: the local path to your image
+    ---------------
+    `);
   }
-
-const universeOwnerAccount = web3.eth.accounts.privateKeyToAccount(pvk);
-const endpoint = api;
-const universeIdx = uni;
-const filePath = path;
+  return OK;
+};
 
 const generateImgHash = async ({ file }) => {
   const fileHash = await Hash.of(file);
-
   return fileHash;
 };
 
-const uploadImage = async ({ filePath, universeIdx }) => {
-  try {
-    const myFileReadStream = createReadStream(filePath);
-    const fileHash = await generateImgHash({ file: myFileReadStream })
+const uploadImage = async ({ filePath, universeIdx, signature }) => {
+  const imageSignature = signature.signature.substr(2, signature.signature.length);
 
-    const signature = signer.signImageUpload({
-      web3account: universeOwnerAccount,
-      fileHash,
-      universeIdx,
-    });
+  const form = new FormData();
+  form.append('universeIdx', universeIdx);
+  form.append('signature', imageSignature);
+  form.append('upload', createReadStream(filePath));
 
-    const form = new FormData();
-    const imageSignature = signature.signature.substr(2, signature.signature.length);
-
-    console.log("Image signature: ", imageSignature);
-    form.append('universeIdx', universeIdx);
-    form.append('signature', imageSignature);
-    form.append('upload', createReadStream(filePath));
-
-    const response = await fetch(`${endpoint}/images/upload`, {
-      method: 'POST',
-      body: form,
-    });
-    const contentType = response.headers.get('Content-Type');
-    if (contentType && contentType.startsWith('application/json')) {
-      return response.json();
-    } else {
-      return response.text();
-    }
-  } catch (err) {
-    throw err;
+  const response = await fetch(`${endpoint}/images/upload`, {
+    method: 'POST',
+    body: form,
+  });
+  const contentType = response.headers.get('Content-Type');
+  if (contentType && contentType.startsWith('application/json')) {
+    return response.json();
   }
-}
+  return response.text();
+};
 
 const run = async () => {
+  // Compute the image IPFS hash, so that it can be uploaded any time (now or future):
+  const myFileReadStream = createReadStream(path);
+  const fileHash = await generateImgHash({ file: myFileReadStream });
+
+  // The query requires the signature of the universe owner:
+  const universeOwnerAccount = identity.accountFromPrivateKey(pvk);
+  const signature = signer.signImageUpload({
+    web3account: universeOwnerAccount,
+    fileHash,
+    universeIdx: universe,
+  });
+
+  // Execute the upload query:
   try {
-    const imageUploaded = await uploadImage({ filePath, universeIdx })
-    console.log("Image Uploaded: ", imageUploaded)
+    const response = await uploadImage({ filePath: path, universeIdx: universe, signature });
+    console.log('Image Uploaded. Query response: ', response);
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 };
 
-run();
+const OK = checkArgs();
+if (OK) run();
