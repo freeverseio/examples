9038c9b918cf6b8a913a17ea00542437a45e82c0
Toni Mateos <tmateos@freeverse.io>
2021-09-29 15:31:55 +0200

    refactoring to use buyNowId

--- nodejs/create_buy_now.js
+++ nodejs/create_buy_now.js
@@ -36,10 +36,10 @@ const {
 
 const checkArgs = () => {
   const OK = (assetId && pvk && currencyId && price && rnd && validUntil);
-  console.log(assetId, pvk, currencyId, price, rnd, validUntil);
   if (!OK) {
     console.log(`
     ---------------
+    Function: puts an asset for sale in BuyNow mode (as opposite to auction) 
     Usage Example: 
     node create_buy_now.js --pvk '0xd2827f4c3778758eb51719a698464aaffd10a5c7cf816c1de83c5e446bfc8e8d' --currencyId 0 --price 345 --rnd 12342234 --validUntil '1632395810' --assetId '36771977682424071759165601888702044610709221343463' 
     ---------------
@@ -48,7 +48,7 @@ const checkArgs = () => {
     * pvk: the private key of the owner of the asset
     * assetId
     * currencyId: currency 0: EUR
-    * price: in units of cents of EUR, so 3.45 EUR
+    * price: in units of cents of EUR, so 345 = 3.45 EUR
     * rnd: a random number, to be generated in front end for each different query
     * validUntil: when will the buynow end (Thursday, 23 September 2021 11:16:50)
     `);
