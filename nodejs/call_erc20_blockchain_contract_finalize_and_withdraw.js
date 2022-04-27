7c136296307beb804bc3272db4aa8de4cd15c9d1
Christian <cristian.cuaresma@gmail.com>
2022-04-27 13:29:42 +0200

    Add finalize and withdraw call

--- nodejs/call_erc20_blockchain_contract_finalize_and_withdraw.js
+++ nodejs/call_erc20_blockchain_contract_finalize_and_withdraw.js
@@ -23,7 +23,7 @@
 
 const Eth = require('web3-eth');
 const identity = require('freeverse-crypto-js');
-const { NativeCryptoPayments } = require('freeverse-marketsigner-js');
+const { ERC20Payments } = require('freeverse-marketsigner-js');
 const argv = require('minimist')(process.argv.slice(2), {
   string: [
     'paymentsAddr',
@@ -31,12 +31,9 @@ const argv = require('minimist')(process.argv.slice(2), {
     'pvk',
     'operatorSig',
     'paymentId',
-    'price',
-    'feeBPS',
-    'universeId',
-    'deadline',
-    'seller',
     'rpcUrl',
+    'wasSuccessful',
+    'erc20Addr'
   ],
 });
 
@@ -46,37 +43,31 @@ const {
   pvk,
   operatorSig,
   paymentId,
-  price,
-  feeBPS,
-  universeId,
-  deadline,
-  seller,
   rpcUrl,
+  wasSuccessful,
+  erc20Addr,
 } = argv;
 
 const checkArgs = () => {
   const OK = (
-    paymentsAddr && confirmationBlock && pvk && operatorSig && paymentId && price && feeBPS && universeId && deadline && seller && rpcUrl
+    paymentsAddr && confirmationBlock && pvk && operatorSig && paymentId && rpcUrl && wasSuccessful && erc20Addr
     );
     if (!OK) {
       console.log(`
       ---------------
-      Function: calls NativePayments blockchain smart contract method 'pay'
+      Function: calls NativePayments blockchain smart contract method 'finalizeAndWithdraw'
       Usage Example: 
-      node call_bluecoin_blockchain_contract_pay.js --pvk 'd2827f4c3778758eb51719a698464aaffd10a5c7cf816c1de83c5e446bfc8e8d' --deadline '1651048702' --feeBPS '50' --paymentId '0x67e536c83928dec5ae68b1cb7bd55c4d22e7252a92d588944839aba7675d40f7'  --operatorSig '0xddbfc28a5d8b4e67af13099a1f5c7d0e2c11b0d1350ade51bb5b8abe2254cb5932a582521a6127e5aaad0eff9fab4bb04a3ac226ecb5605b669edb9a01a5fdc11b' --price '10' --seller '0x65bf60D431AB6aBd96F4a4Ef32C106d6B5761C27' --universeId '0' --paymentsAddr '0xe1bfcc5fA429c84f73C684728549A15105C74970' --confirmationBlock 8 --rpcUrl 'https://matic-mumbai.chainstacklabs.com'
+      node call_erc20_blockchain_contract_finalize_and_withdraw.js --pvk 'd2827f4c3778758eb517cf816c1de83c5e446bfc8e8d' --paymentId '0x67e536c83928dec5ae68b1cb7bd55c4d22e7252a92d588944839aba7675d40f7'  --operatorSig '0xddbfc28a5d8b4e67af13099a1f5c7d0e2c11b0d1350ade51bb5b8abe2254cb5932a582521a6127e5aaad0eff9fab4bb04a3ac226ecb5605b669edb9a01a5fdc11b' --confirmationBlock 8 --paymentsAddr '0xe1bfcc5fA429c84f73C684728549A15105C74970' --rpcUrl 'https://matic-mumbai.chainstacklabs.com' --wasSuccessful true --erc20Addr '0xe1bfcc5fA429c84f73C68412349A15105C74970'
       ---------------
       
       params:
-    * pvk: the private key of the buyer
-    * paymentsAddr: the address of the smart contract that acts as escrow
-    * confirmationBlock: number of blocks to wait after tx hash has been mined to consider it as confirmed
-    * operatorSig: signature that FV issues that allows the buyer address to call the contract method pay returned by the create_buynow_payment mutation
-    * paymentId: id returned by the create_buynow_payment mutation
-    * price: price returned by the create_buynow_payment mutation
-    * feeBPS: feeBPS returned by the create_buynow_payment mutation
-    * universeId: universeId returned by the create_buynow_payment mutation
-    * deadline: returned by create_buynow_payment mutation
-    * seller: returned by create_buynow_payment mutation
+      * pvk: the private key of the seller
+      * operatorSig: signature that FV issues that allows the seller address to call the contract method finalizeAndWithdraw returned by the cashout mutation
+      * erc20Addr: the address of the ERC20 Token contract
+      * paymentId: id returned by the cashout mutation
+      * wasSuccessful: boolean returned by cashout mutation
+      * paymentsAddr: the address of the smart contract that acts as escrow
+      * confirmationBlock: number of blocks to wait after tx hash has been mined to consider it as confirmed
       `);
     }
     return OK;
@@ -96,30 +87,24 @@ const checkArgs = () => {
   };
   
   const run = async () => {
-    // Note: before doing anything related to asset trading
+  // Note: before doing anything related to asset trading
   // the user's ID needs to be registered.
   // This registration needs to be done only once
   // for a given user's ID.
   // See the link_id_to_email.js examples
   
   const eth = new Eth('https://matic-mumbai.chainstacklabs.com');
-  const buyerAccount = identity.accountFromPrivateKey(pvk);
+  const sellerAccount = identity.accountFromPrivateKey(pvk);
   eth.accounts.wallet.add(pvk) // Only needed because we are not using metamask or another provider that holds the pvk of the sender
   
+  const paymentsInstance = new ERC20Payments({ paymentsAddr, eth, confirmationBlock, erc20Addr })
 
-  const paymentsInstance = new NativeCryptoPayments({ paymentsAddr, eth, confirmationBlock })
-
-  const paymentData = {
+  const assetTransferData = {
     paymentId,
-    amount: price.toString(),
-    feeBPS: feeBPS.toString(),
-    universeId: universeId.toString(),
-    deadline: deadline.toString(),
-    buyer: buyerAccount.address,
-    seller: seller,
+    wasSuccessful,
   };
   
-  paymentsInstance.pay({ paymentData, signature: operatorSig, from: buyerAccount.address })
+  paymentsInstance.finalizeAndWithdraw({ assetTransferData, signature: operatorSig, from: sellerAccount.address })
   .once('receipt', _onReceiptHandler)
   .on('confirmation', _onConfirmationHandler)
   .on('error', (err) => {
