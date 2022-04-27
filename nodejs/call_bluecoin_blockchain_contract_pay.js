61cfa75873f13a61222a2bc33731fafaed94f49c
Toni Mateos <tmateos@freeverse.io>
2022-04-27 17:22:37 +0200

    fixing help lines

--- nodejs/call_bluecoin_blockchain_contract_pay.js
+++ nodejs/call_bluecoin_blockchain_contract_pay.js
@@ -65,22 +65,28 @@ const checkArgs = () => {
   if (!OK) {
     console.log(`
       ---------------
-      Function: calls NativePayments blockchain smart contract method 'pay'
+      Function: Sends a payment TX to the escrow Smart Contract 
       Usage Example: 
-      node call_bluecoin_blockchain_contract_pay.js --pvk 'd2827f4c3778758eb51719a698464aaffd10a5c7cf816c1de83c5e446bfc8e8d' --deadline '1651048702' --feeBPS '50' --paymentId '0x67e536c83928dec5ae68b1cb7bd55c4d22e7252a92d588944839aba7675d40f7'  --operatorSig '0xddbfc28a5d8b4e67af13099a1f5c7d0e2c11b0d1350ade51bb5b8abe2254cb5932a582521a6127e5aaad0eff9fab4bb04a3ac226ecb5605b669edb9a01a5fdc11b' --price '10' --seller '0x65bf60D431AB6aBd96F4a4Ef32C106d6B5761C27' --universeId '0' --paymentsAddr '0xe1bfcc5fA429c84f73C684728549A15105C74970' --confirmationBlock 8 --rpcUrl 'https://matic-net.chainstacklabs.com'
+      node call_bluecoin_blockchain_contract_pay.js --pvk 'd2827f4c3778758eb51719a698464aaffd10a5c7cf816c1de83c5e446bfc8e8d' --deadline '1651048702' --feeBPS '50' --paymentId '0x67e536c83928dec5ae68b1cb7bd55c4d22e7252a92d588944839aba7675d40f7'  --operatorSig '0xddbfc28a5d8b4e67af13099a1f5c7d0e2c11b0d1350ade51bb5b8abe2254cb5932a582521a6127e5aaad0eff9fab4bb04a3ac226ecb5605b669edb9a01a5fdc11b' --price '10' --seller '0x65bf60D431AB6aBd96F4a4Ef32C106d6B5761C27' --universeId '0' --paymentsAddr '0xe1bfcc5fA429c84f73C684728549A15105C74970' --confirmationBlock 8 --rpcUrl 'https://matic-net.chainstacklabs.com' --chainId 80001
       ---------------
       
       params:
-    * pvk: the private key of the buyer
-    * paymentsAddr: the address of the smart contract that acts as escrow
-    * confirmationBlock: number of blocks to wait after tx hash has been mined to consider it as confirmed
-    * operatorSig: signature that FV issues that allows the buyer address to call the contract method pay returned by the create_buynow_payment mutation
-    * paymentId: id returned by the create_buynow_payment mutation
-    * price: price returned by the create_buynow_payment mutation
-    * feeBPS: feeBPS returned by the create_buynow_payment mutation
-    * universeId: universeId returned by the create_buynow_payment mutation
-    * deadline: returned by create_buynow_payment mutation
-    * seller: returned by create_buynow_payment mutation
+      * pvk: the private key of the buyer
+
+      params related to the blockchain where the escrow contract is deployed:
+      * rpcUrl: the url of a (public) node of that blockchain
+      * chainId: the chainId of the blockchain
+      * paymentsAddr: the address of the smart contract that acts as escrow
+      * confirmationBlock: number of blocks that the provider waits after tx hash has been mined to call the onConfirmed method
+    
+      params related to the payment transaction, all of them returned when calling the create_buynow_payment mutation
+      * paymentId: id that identifies the payment in the escrow contract
+      * price: price of the buynow, in lowest possible units of the cryptocurrency accepted by the escrow contract
+      * feeBPS: the fee charged to the seller, in basis points (e.g. 250 would correspond to 2.5%)
+      * universeId: universeId where this asset belongs
+      * deadline: the time in UTC seconds after which the payment will not be accepted by the escrow contract
+      * seller: the seller of the asset
+      * operatorSig: the signature that FV issues, which certifies all of the params above.
       `);
   }
   return OK;
