6968b6c58cb7cd48e52b53e99e687cc5c48bccbb
Toni Mateos <tmateos@freeverse.io>
2023-03-22 11:35:23 +0100

    rephrasing and minor corrections

--- nodejs/unlink_id_to_email.js
+++ nodejs/unlink_id_to_email.js
@@ -1,22 +1,24 @@
 /* eslint-disable no-console */
 
 /*
-Unlinks a previously linked web3address from an email
+Unlinks a previously linked web3address from an email.
+The unlinked web3address can later be assigned to a different email.
+NOTE: This mutation needs to include an authorization b2b token in the http header.
 
 INPUTS:
 - email: the email to be unlinked
 - web3Address: the web3 address to be unlinked
-- signature: the signature of web3 address authorizing the process of linking it to the email
+- signature: the signature by the web3 address authorizing the process of unlinking it from an email
 */
 const identity = require('freeverse-crypto-js');
 const { digestUnlinkAddress, sign } = require('freeverse-marketsigner-js');
 
-// In this example, a web3 account is created from scratch, so that
-// the user can sign. You can use your own web3 wallet in your app.
-const pvk = '0x56450b9e335eb41b0c90454285001f793e7bac2b2c94c353c392b38a2292e7d0';
 const email = 'john@ama.com';
 
-const userAccount = identity.accountFromPrivateKey(pvk);
+// In this example, a web3 account is created from scratch, so that
+// the user can sign. You can use your own web3 wallet in your app.
+const randomPvk = '0x56450b9e335eb41b0c90454285001f793e7bac2b2c94c353c392b38a2292e7d0';
+const userAccount = identity.accountFromPrivateKey(randomPvk);
 const userWeb3Address = userAccount.address;
 
 // Compute the digest to be signed:
@@ -38,5 +40,3 @@ mutation{
   }
 `;
 console.log(unlinkWeb3AddressMutation);
-
-// This mutation needs to be authorized with an http header b2b token
